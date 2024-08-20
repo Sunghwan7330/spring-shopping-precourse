6931fd0eead801879ec99fb95517ff696fa3977d
sunghwan <35713051+Sunghwan7330@users.noreply.github.com>
2024-08-20 10:35:05 +0900

    feat: GET, DELETE 요청 추가 (#2)

--- src/main/java/shopping/controller/ProductController.java
+++ src/main/java/shopping/controller/ProductController.java
@@ -1,13 +1,13 @@
 package shopping.controller;
 
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import shopping.ProductManager;
 import shopping.dto.AddProductRequestDto;
+import shopping.entity.Product;
 
 @RestController
 public class ProductController {
@@ -21,5 +21,26 @@ public class ProductController {
     public ResponseEntity<Boolean> addProduct(@RequestBody AddProductRequestDto request) {
         boolean res = productManager.addProduct(request.toProductDto());
         return ResponseEntity.status(200).body(res);
+    }
+    @GetMapping("/api/products")
+    public ResponseEntity<Product> getData(@RequestParam(name = "name", defaultValue = "") String name) {
+        Product product = productManager.getExistName(name);
+
+        if (product == null) {
+            return ResponseEntity.status(404).body(null);
+        }
+
+        return ResponseEntity.status(200).body(product);
+    }
+
+    @DeleteMapping("/api/products")
+    public ResponseEntity<Boolean> deleteData(@RequestParam(name = "name", defaultValue = "") String name) {
+        Product product = productManager.getExistName(name);
+
+        if (product == null) {
+            return ResponseEntity.status(404).body(null);
+        }
+
+        return ResponseEntity.status(200).body(true);
     }
 }
